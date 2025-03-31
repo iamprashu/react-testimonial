edd373a087ab41d72c5670523212fc1e44c5ea63
iamprashu <iamprashu.gs@gmail.com>
2025-03-31 21:56:37 +0530

    Backend Data Hardcoaded

---
+++ src/data.jsx
@@ -0,0 +1,39 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Lakshay Sharma",
+    job: "Machine Learning Engineer",
+    image: "./src/assets/01.png",
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+  {
+    id: 2,
+    name: "AbAbhishek Sharma",
+    job: "Graphic Designer",
+    image: "./src/assets/02.png",
+    text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+  },
+  {
+    id: 3,
+    name: "Chandra Shekhar Pandey",
+    job: "Senior Software Engineer",
+    image: "./src/assets/04.png",
+    text: "I am thrilled with the results of our recent marketing campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+  },
+  {
+    id: 4,
+    name: "Chetan Verma",
+    job: "Full Stack Developer",
+    image: "./src/assets/03.png",
+    text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+  },
+  {
+    id: 5,
+    name: "Vijay Badsila",
+    job: "Data Analyst",
+    image: "./src/assets/05.png",
+    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+];
+
+export default reviews;
